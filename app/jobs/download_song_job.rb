d4a97dbade43c928b1d0027dbd76cbb62af9d255
Joseph Utecht <thenmal@gmail.com>
2023-11-28 14:33:12 -0800

    song info

--- app/jobs/download_song_job.rb
+++ app/jobs/download_song_job.rb
@@ -6,10 +6,8 @@ class DownloadSongJob < ApplicationJob
 
   def perform(*args)
     @song = Song.find(args[0])
-    puts @song.inspect
     YoutubeDL::Command.config.default_options = { format: 'mp4' }
     video = YoutubeDL.download(@song.link).call
-    puts video.pretty_inspect
     duration_string = video.info['duration_string']
     # convert minutes:seconds to seconds
     duration = duration_string.split(':').map(&:to_i).inject(0) { |a, b| a * 60 + b }
@@ -20,6 +18,7 @@ class DownloadSongJob < ApplicationJob
     thumbnail_file = URI.open(thumbnail_url)
 
     @song.update(title: video_title, length: duration)
+    @song.save
 
     # Create a blob for the thumbnail
     blob = ActiveStorage::Blob.create_and_upload!(
@@ -29,6 +28,7 @@ class DownloadSongJob < ApplicationJob
 
     # Attach the blob to your model
     @song.thumbnail.attach(blob)
+    @song.save
 
     blob = ActiveStorage::Blob.create_and_upload!(
       io: File.open(video.destination),
